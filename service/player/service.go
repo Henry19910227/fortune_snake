7228dcfd257b7f68c3f36dde52f50ebd9c2163b4
Henry <>
2025-04-23 18:30:48 +0800

    feat: 新增 player service

---
+++ service/player/service.go
@@ -0,0 +1,36 @@
+package player
+
+import (
+	"errors"
+	"game_server_slots_fortune_snake/constants"
+	"game_server_slots_fortune_snake/model/player/get_player_session"
+	"game_server_slots_fortune_snake/pkg"
+	playerRepo "game_server_slots_fortune_snake/repository/player"
+	"github.com/redis/go-redis/v9"
+)
+
+type service struct {
+	playerRepo playerRepo.Repository
+}
+
+func NewService(playerRepo playerRepo.Repository) Service {
+	return &service{playerRepo: playerRepo}
+}
+
+func (s *service) GetPlayerSession(input *get_player_session.Input) (output *get_player_session.Output) {
+	data, err := s.playerRepo.FindPlayerSessionById(input.Ctx, input.PlayerId)
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			output.Code = constants.CodeUnauthorized
+			output.Message = pkg.LocalizeInstance().LocalizeMessage("TokenExpired")
+			return output
+		}
+		output.Code = constants.CodeBadRequest
+		output.Message = pkg.LocalizeInstance().LocalizeMessage("RedisError", map[string]interface{}{"err": err})
+	}
+	output = &get_player_session.Output{}
+	output.Code = constants.CodeSuccess
+	output.Message = ""
+	output.Data = data
+	return output
+}
