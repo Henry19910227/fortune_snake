9336823b2d07b2f31a42a85502f4567289cdcb5d
Henry <>
2025-04-24 16:08:24 +0800

    feat: 新增 grpc 測試客戶端

--- grpc_client/main.go
+++ grpc_client/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc_client/proto"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	conn, err := grpc.Dial("127.0.0.1:8181", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		fmt.Printf("did not connect: %v\n", err)
+	}
+	defer conn.Close()
+	client := proto.NewMessageServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	resp, err := client.SendMessage(ctx, &proto.MessageRequest{Action: "player", PlayerId: 10376293541461622785, Data: ""})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(resp.Code, resp.Message, resp.Data)
 }
